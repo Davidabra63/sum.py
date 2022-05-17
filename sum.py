8229fdceb85985b4364c502e278514d729bc0dcb
David Abraham <105358890+Davidabra63@users.noreply.github.com>
2022-05-17 07:01:05 -0700

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,10 @@
+def add(x,y):
+    result = x+y
+    return result
+
+n1 = int(input("enter the first number:"))
+n2 = int(input("enter the second number:"))
+
+res = add(n1,n2)
+print(n1,"+",n2,"=",res)
+         
